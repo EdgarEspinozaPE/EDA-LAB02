2a89f02a29f429453925060e7ea8c8382a2d4719
EdgarEspinozaPE <64268406+EdgarEspinozaPE@users.noreply.github.com>
2021-11-03 18:33:16 -0500

    Implementación button insert

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 let quadtree;
 let count = 0;
+let button1;
+let canvas1;
+let inpx;
+let inpy;
+
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(800, 600);
     let boundary = new Rectangle(300, 300, 300, 300);
     quadtree = new QuadTree(boundary, 4);
     console.log(quadtree);
@@ -22,4 +27,24 @@ function setup() {
         point(p.x, p.y);
     }
 
+    //button experiments
+    button1 = createButton('Insertar');
+    button1.position(670, 80);
+    button1.mousePressed(insertpoint);
+
+    inpx = createInput('x');
+    inpx.position(640, 120);
+    inpx.size(50);
+
+    inpy = createInput('y');
+    inpy.position(700, 120);
+    inpy.size(50);
+
+}
+
+function insertpoint() {
+    let p = new Point(inpx.value(), inpy.value());
+    quadtree.insert(p);
+    quadtree.show();
+    console.log('you are typing: ', inpx.value());
 }
